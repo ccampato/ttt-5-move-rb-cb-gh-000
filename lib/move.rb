2882f946ff2a111ec6152d5ead8453f02bbd9a07
Caio Sanches <ccampato@gmail.com>
2018-04-06 00:29:55 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,8 +9,8 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input = ["1","2","3","4","5","6","7","8","9"]
-  converted_input = user_input[].to_i
+  user_input = " ".to_i
+  converted_input = input_to_index(user_input)
   index = user_input - 1
 end
 

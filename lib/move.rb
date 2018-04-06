9711d0ed188d98919f433ee77265d44695433cf8
Caio Sanches <ccampato@gmail.com>
2018-04-06 00:09:39 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ end
 
 def input_to_index(user_input)
   user_input = " "
-  converted_input = input_to_index(user_input).to_i
+  converted_input = user_input.to_i
 end
 
 def move(array,index,value)

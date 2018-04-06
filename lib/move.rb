6d6a51774bd528ac71d961b57464763c20338fdf
Caio Sanches <ccampato@gmail.com>
2018-04-06 00:44:18 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,8 +9,7 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input = " ".to_i
-  converted_input = user_input-1
+  converted_input = user_input.to_i!-1
 end
 
 def move(array,index,value)

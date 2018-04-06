d10e9b676dd5e3978afe35ee602e845ea49d1200
Caio Sanches <ccampato@gmail.com>
2018-04-06 00:33:17 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,8 +9,8 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input = " ".to_i
-  converted_input = input_to_index(user_input)
+  user_input = " "
+  converted_input = user_input.to_i
   index = user_input - 1
 end
 
